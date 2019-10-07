2c256faf38ff6fe7526c0b9ec0e47de0537a7912
Erick Maria <erickmaria08@gmail.com>
2019-10-06 23:31:16 -0300

    add new terraform outputs

--- azure/virtualmachine/output.tf
+++ azure/virtualmachine/output.tf
@@ -2,7 +2,23 @@ output "public_ip_address_server" {
   value = "${data.azurerm_public_ip.data_public_ip_server.ip_address}"
 }
 
+output "server_user"{
+  value = "${var.server_admin_user}"
+}
+
+output "server_pass" {
+  value = "${var.server_admin_pass}"
+}
 
 output "public_ip_address_host" {
   value = "${data.azurerm_public_ip.data_public_ip_host.ip_address}"
 }
+
+output "host_user"{
+  value = "${var.host_admin_user}"
+}
+
+output "host_pass" {
+  value = "${var.host_admin_pass}"
+}
+
